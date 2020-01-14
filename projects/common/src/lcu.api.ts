b745ed39277fa39a61f22be4b9b311b01075e683
Robert Wise <robert.wise@fathym.com>
2020-01-14 12:04:29 -0700

    Initial merge/implementation of Ngx-Charts code

--- projects/common/src/lcu.api.ts
+++ projects/common/src/lcu.api.ts
@@ -1,7 +1,59 @@
 export * from './lib/lcu-charts.module';
-export * from './lib/controls/lcu/lcu.component';
-export * from './lib/directives/lcu.directive';
-export * from './lib/models/lcu.model';
-export * from './lib/services/lcu.service';
-export * from './lib/state/lcu-manager.context';
-export * from './lib/utils/lcu.utils';
+
+/** COMMON */
+export * from './lib/common/chart-common.module';
+export * from './lib/common/legend/legend.component';
+export * from './lib/common/legend/scale-legend.component';
+export * from './lib/common/legend/legend-entry.component';
+export * from './lib/common/legend/advanced-legend.component';
+export * from './lib/common/tooltip/tooltip.module';
+export * from './lib/common/tooltip/tooltip.service';
+export * from './lib/common/tooltip/tooltip.component';
+export * from './lib/common/tooltip/tooltip.directive';
+export * from './lib/common/tooltip/style.type';
+export * from './lib/common/tooltip/alignment.type';
+export * from './lib/common/tooltip/show.type';
+export * from './lib/common/axes/axes.module';
+export * from './lib/common/axes/axis-label.component';
+export * from './lib/common/axes/x-axis.component';
+export * from './lib/common/axes/x-axis-ticks.component';
+export * from './lib/common/axes/y-axis.component';
+export * from './lib/common/axes/y-axis-ticks.component';
+export * from './lib/common/axes/ticks.helper';
+export * from './lib/common/count/count.directive';
+export * from './lib/common/count/count.helper';
+export * from './lib/common/timeline/timeline.component';
+export * from './lib/common/color.helper';
+export * from './lib/common/charts/chart.component';
+export * from './lib/common/area.component';
+export * from './lib/common/base-chart.component';
+export * from './lib/common/circle.component';
+export * from './lib/common/circle-series.component';
+export * from './lib/common/grid-layout.helper';
+export * from './lib/common/grid-panel.component';
+export * from './lib/common/grid-panel-series.component';
+export * from './lib/common/svg-linear-gradient.component';
+export * from './lib/common/svg-radial-gradient.component';
+export * from './lib/common/tooltip-area.component';
+export * from './lib/common/tick-format.helper';
+export * from './lib/common/trim-label.helper';
+export * from './lib/common/view-dimensions.helper';
+export * from './lib/common/label.helper';
+export * from './lib/common/domain.helper';
+
+/** LINE CHART */
+export * from './lib/controls/line-chart/line-chart.module';
+export * from './lib/controls/line-chart/line-chart.component';
+export * from './lib/controls/line-chart/line.component';
+export * from './lib/controls/line-chart/line-series.component';
+
+/** MODELS */
+export * from './lib/models/chart-data.model';
+
+/** UTILS */
+export * from './lib/utils/id';
+export * from './lib/utils/color-sets';
+export * from './lib/utils/sort';
+export * from './lib/utils/throttle';
+export * from './lib/utils/color-utils';
+export * from './lib/utils/visibility-observer';
